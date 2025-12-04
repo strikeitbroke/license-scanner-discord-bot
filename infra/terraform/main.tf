04d50d5e2aa2d4ffc7838e757aaeb9418b020723
strikeitbroke <strikeitbroke@gmail.com>
2025-12-03 23:53:40 -0800

    added workflow

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,5 +1,42 @@
+variable "discord_token" {
+  description = "The Discord bot token"
+  type        = string
+  sensitive   = true
+}
+
+variable "pr_api_token" {
+  description = "PR API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "channel_id" {
+  description = "PR API token"
+  type        = string
+  sensitive   = true
+}
+
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-firewall"
+  description = "allow tls"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_instance" "license_scanner_bot" {
@@ -20,9 +57,9 @@ resource "aws_instance" "license_scanner_bot" {
 
       # Install Docker
       amazon-linux-extras install docker -y || yum install docker -y
+      sudo usermod -aG docker ec2-user
       systemctl start docker
       systemctl enable docker
-      sudo usermod -aG docker ec2-user
 
       # Login to ECR (uses IAM role)
       aws ecr get-login-password --region us-east-1 \
@@ -32,27 +69,12 @@ resource "aws_instance" "license_scanner_bot" {
       docker pull 390403880719.dkr.ecr.us-east-1.amazonaws.com/license_scanner_bot:v1.0.0
 
       # Run the container
-      docker run -d --name license_scanner_bot 390403880719.dkr.ecr.us-east-1.amazonaws.com/license_scanner_bot:v1.0.0
+      docker run -d --name license_scanner_bot \
+        -e DISCORD_TOKEN="${var.discord_token}" \
+        -e CHANNEL_ID="${var.channel_id}" \
+        -e PR_API_TOKEN="${var.pr_api_token}" \
+        390403880719.dkr.ecr.us-east-1.amazonaws.com/license_scanner_bot:v1.0.0 
     EOF
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "terraform-firewall"
-  description = "allow tls"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_ecr_repository" "license_scanner_bot" {
